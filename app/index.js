e714cc1ebed44ed1c206c95d127c1e08f1697ab1
Ivanq <imachug@yandex.ru>
2017-10-17 16:02:27 +0300

    Move handler to separate function in GUI

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 let electron = require("electron");
+let fs = require("fs");
+let api = require("../api.js");
 
 let dragndrop = document.getElementById("dragndrop");
 
@@ -12,6 +14,48 @@ document.body.ondragleave = e => {
 	e.preventDefault();
 };
 
+function handle(fcpXmlPath) {
+	let srtPath = fcpXmlPath.lastIndexOf("/") < fcpXmlPath.lastIndexOf(".") ? fcpXmlPath.replace(/\.(.*?)$/, ".srt") : fcpXmlPath + ".srt";
+
+	return new Promise((resolve, reject) => {
+		fs.stat(srtPath, (e, stats) => {
+			if(!e) {
+				electron.remote.dialog.showMessageBox({
+					type: "warning",
+					buttons: ["No", "Yes"],
+					defaultId: 0,
+					title: "FCPXML to SRT",
+					message: "File " + srtPath + " already exists. Would you like to overwrite it?",
+					cancelId: 0
+				}, response => {
+					if(response == 1) {
+						resolve(api(fcpXmlPath, srtPath));
+					} else {
+						resolve(false);
+					}
+				});
+				return;
+			}
+
+			resolve(api(fcpXmlPath, srtPath));
+		});
+	})
+		.catch(e => {
+			return new Promise((resolve, reject) => {
+				electron.remote.dialog.showMessageBox({
+					type: "error",
+					buttons: ["OK"],
+					defaultId: 0,
+					title: "FCPXML to SRT",
+					message: e.message,
+					cancelId: 0
+				}, response => {
+					resolve(false);
+				});
+			});
+		});
+}
+
 dragndrop.ondrop = e => {
 	e.preventDefault();
 
@@ -19,72 +63,26 @@ dragndrop.ondrop = e => {
 		return;
 	}
 
-	let fs = require("fs");
-	let api = require("../api.js");
-
 	let files = Array.from(e.dataTransfer.files);
+	let list = [];
 
-	let promises = files.map(file => {
-		let fcpXmlPath = file.path;
-		let srtPath = file.path.lastIndexOf("/") < file.path.lastIndexOf(".") ? file.path.replace(/\.(.*?)$/, ".srt") : file.path + ".srt";
-
-		return new Promise((resolve, reject) => {
-			fs.stat(srtPath, (e, stats) => {
-				if(!e) {
-					electron.remote.dialog.showMessageBox({
-						type: "warning",
-						buttons: ["No", "Yes"],
-						defaultId: 0,
-						title: "FCPXML to SRT",
-						message: "File " + srtPath + " already exists. Would you like to overwrite it?",
-						cancelId: 0
-					}, response => {
-						if(response == 1) {
-							resolve(api(fcpXmlPath, srtPath));
-						} else {
-							let index = files.indexOf(file);
-							if(index > -1) {
-								files.splice(index, 1);
-							}
-
-							resolve(false);
-						}
-					});
-					return;
-				}
-
-				resolve(api(fcpXmlPath, srtPath));
-			});
-		})
-			.catch(e => {
-				return new Promise((resolve, reject) => {
-					electron.remote.dialog.showMessageBox({
-						type: "error",
-						buttons: ["OK"],
-						defaultId: 0,
-						title: "FCPXML to SRT",
-						message: e.message,
-						cancelId: 0
-					}, response => {
-						resolve(false);
-					});
-
-					let index = files.indexOf(file);
-					if(index > -1) {
-						files.splice(index, 1);
+	Promise.all(
+		files.map(file => {
+			return handle(file.path)
+				.then(res => {
+					if(res) {
+						list.push(file.path);
 					}
 				});
-			});
-	});
-
-	Promise.all(promises).then(() => {
-		if(files.length) {
+		})
+	).then(() => {
+		if(list.length) {
 			electron.remote.dialog.showMessageBox({
 				type: "info",
 				buttons: ["OK"],
 				defaultId: 0,
 				title: "FCPXML to SRT",
-				message: files.length + " " + (files.length == 1 ? "file was" : "files were") + " created.",
+				message: list.length + " " + (list.length == 1 ? "file was" : "files were") + " created.",
 				cancelId: 0
 			});
 		}

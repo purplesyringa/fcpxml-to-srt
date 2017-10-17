edd8be08a06b4b3e603ef5646dfb438f7357accf
Ivanq <imachug@yandex.ru>
2017-10-17 19:19:21 +0300

    Add icon, remove resize and add platform-specific builds

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ let electron = require("electron");
 electron.app.on("ready", () => {
 	let win = new electron.BrowserWindow({
 		width: 800,
-		height: 600
+		height: 600,
+		icon: path.join(__dirname, "icon.png"),
+		resizable: false
 	});
 	win.setMenu(null);
 
